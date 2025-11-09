65079db2d920c3dbe774b06ce6cf1f362eeb5273
ICEturbo <m.pijnenburg2008@gmail.com>
2025-11-09 21:58:49 +0100

    Finish assortiment page

---
+++ pages/script.js
@@ -0,0 +1,32 @@
+const openPopupSidebar = document.getElementById("openPopupSidebar");
+const openPopupHeader = document.getElementById("openPopupHeader");
+const closePopup = document.getElementById("closePopup");
+const popup = document.getElementById("popup");
+const viewProducts = document.getElementById("viewProducts");
+
+openPopupSidebar.onclick = function() {
+    popup.classList.add("active");
+}
+openPopupHeader.onclick = function() {
+    popup.classList.add("active");
+}
+
+viewProducts.onclick = function() {
+    // bijvoorbeeld: popup sluiten en naar een pagina scrollen
+    popup.classList.remove("active");
+    window.location.href = "assortiment.html"; // of jouw gewenste link
+}
+
+closePopup.onclick = function() {
+    popup.classList.remove("active");
+}
+
+window.onclick = function(event) {
+    if (event.target == popup) {
+        popup.classList.remove("active");
+    }
+}
+
+function toggleMenu() {
+    document.getElementById('sideMenu').classList.toggle('active');
+}
